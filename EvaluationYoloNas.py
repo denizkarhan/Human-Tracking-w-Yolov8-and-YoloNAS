e717f7846bfd0dc85d6aefcbe2f6223903d90e14
Deniz Karhan <81527587+denizkarhan@users.noreply.github.com>
2023-12-30 15:26:13 +0300

    Delete EvaluationYoloNas.py

--- EvaluationYoloNas.py
+++
@@ -1,9 +0,0 @@
-from super_gradients.training import models
-import cv2
-import torch
-
-device = 'cuda' if torch.cuda.is_available() else "cpu"
-model = models.get("yolo_nas_s", pretrained_weights="coco")
-
-img_path = r"messi_ronaldo.jpg"
-model.predict(img_path, conf=0.5).show()
